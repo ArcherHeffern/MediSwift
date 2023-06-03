fbcd7ec5809781cf0aa498481aa038fb679c248d
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-03 16:56:03 -0400

    card grid created and styled

--- src/card.tsx
+++ src/card.tsx
@@ -1,14 +1,29 @@
 import "./card.css";
 
-function Card() {
+interface Props {
+    name: string;
+    quantity: number;
+    price: number;
+    img: string;
+}
 
+function Card(props: Props) {
+    const { name, quantity, price, img } = props;
     return (
-        <>
-        <div className="card-wrapper">
-            this is a card
+        <div className="card-grid">
+            <div className="card-wrapper">
+                <h3 className="card-name">{name || "drug"}</h3>
+                <div className="card-image">
+                    <img src={img} alt={name} />
+                </div>
+                <div className="card-detail" >
+                    <p>Quantity: {quantity}</p>
+                    <p>Price: {price}</p>
+                </div>
+            </div>
         </div>
-        </>
-    
+
+
     );
 
 }
