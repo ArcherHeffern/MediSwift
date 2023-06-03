4f4b4589bea7ad14fb0fb851047f82a7a06b9922
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-03 19:58:12 -0400

    seller page created

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import Card from './card.tsx'
 import data from './tempDB.ts';
 
 function App() {
+  const [isSeller, setIsSeller] = useState(false);
+
   return (
     <>
     <header>
       <h1>Logo</h1>
+      <button onClick={() => {
+        setIsSeller(!isSeller)
+      }}>Switch</button>
     </header>
     <div className='card-container'>
     { 
     data.map((item) => {
-      return <Card {...item}/>
+      return <Card {...item} isSeller={isSeller}/>
     })
   } 
     </div>
--- src/card.tsx
+++ src/card.tsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import "./card.css";
 
 interface Props {
     name: string;
     quantity: number;
     price: number;
-    img: string;
+    isSeller: boolean;
 }
 
 function Card(props: Props) {
-    const { name, quantity, price, img } = props;
+    const { name, quantity, price, isSeller } = props;
+    const [currentQuantity, setCurrentQuantity] = useState(quantity);
+    function incrementQuantity() {
+        setCurrentQuantity(currentQuantity + 1);
+    }
+
+    function decrementQuantity() {
+        if (currentQuantity === 0) return;
+        setCurrentQuantity(currentQuantity - 1);
+    }
     return (
         <div className="card-grid">
             <div className="card-wrapper">
                 <h3 className="card-name">{name || "drug"}</h3>
                 <div className="card-image">
-                    <img src={img} alt={name} />
                 </div>
                 <div className="card-detail" >
-                    <p>Quantity: {quantity}</p>
                     <p>Price: {price}</p>
+                    {isSeller && <button onClick={incrementQuantity}>Add</button>}
+                    <p>Quantity: {currentQuantity}</p>
+                    {isSeller && <button onClick={decrementQuantity}>Remove</button>}
                 </div>
             </div>
         </div>
