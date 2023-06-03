280d1a96ed4b3aa9457dec1f6d5c38ce3b4e1b5f
ArcherHeffern <heffernarcher@gmail.com>
2023-06-03 16:45:10 -0400

    set up layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './card.tsx'
+import data from './tempDB.ts';
 
 function App() {
-
   return (
     <>
     <header>
       <h1>Logo</h1>
     </header>
     <div className='card-container'>
-    {
-      <Card />
-    }
+    { 
+    data.map((item) => {
+      return <Card {...item}/>
+    })
+  } 
     </div>
     </>
   )
