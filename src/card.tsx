0d232748fe5f0c13f5ff597e092a095d60090702
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-03 15:26:49 -0400

    card created

---
+++ src/card.tsx
@@ -0,0 +1,19 @@
+import "card.css";
+
+function Card() {
+
+    return (
+        <>
+        <div>
+            this is a card
+        </div>
+        </>
+    
+    );
+
+}
+
+
+export default Card;
+
+
