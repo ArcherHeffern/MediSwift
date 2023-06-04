42ed6ee2a6b9d2f770f0eff5e6a0d857ddaf2d58
ArcherHeffern <heffernarcher@gmail.com>
2023-06-03 23:30:34 -0400

    added update drug

--- backend/controllers/drugController.ts
+++ backend/controllers/drugController.ts
@@ -1,4 +1,5 @@
-import Drug from '../models/drug';  
+import Drug from '../models/drug';
+import { Request, Response } from 'express';
 
 export const getDrugs = async (req, res) => {
     try {
@@ -20,8 +21,21 @@ export const getDrugs = async (req, res) => {
     }
 };
 
-// export const updateDrug = async (req, res) => {
-// }
+export const updateDrug = async (req: Request, res: Response) => {
 
+    const drug = await Drug.findOne({ where: { id: req.params.id } });
+    if (!drug) {
+        return res.status(404).json({ msg: 'Drug not found.' });
+    }
 
+    const { quantity } = req.body;
+    drug.quantity = quantity;
 
+    try {
+        const updatedDrug = await drug.save();
+        return res.status(200).json(updatedDrug);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: 'Internal server error.' });
+    }
+}
